fb0b8d3ff5559b92438167b9a91b5e01afbfb5d2
pazitschrecker <pazitrose1999@gmail.com>
2019-12-24 09:12:44 -0500

    Updated email link to my email to check

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "spn8@spondonit.com";
+    $to = "pazitrose1999@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
@@ -16,7 +16,7 @@
     $subject = "You have a message from your Bitmap Photography.";
 
     $logo = 'img/logo.png';
-    $link = 'pazitrose1999@gmail.com';
+    $link = '#';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
